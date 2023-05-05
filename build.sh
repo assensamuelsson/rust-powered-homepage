2633f3152cf498ad67abb80ac137bcd3a6d3ff1c
Andreas Samuelsson <andreas.samuelsson@bonniernews.se>
2023-05-05 10:31:39 +0200

    Try new build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-trunk build -d docs --release
-gsed -i 's#/rust-powered-homepage#/rust-powered-homepage/rust-powered-homepage#g' docs/index.html
+trunk build -d docs --release --public-url '/rust-powered-homepage/'
+#gsed -i 's#/rust-powered-homepage#/rust-powered-homepage/rust-powered-homepage#g' docs/index.html
